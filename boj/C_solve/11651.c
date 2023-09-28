2c6a1117bbd137e7edb6734c862f84b4a3f4183f
usfree <tlrmsjtm77@naver.com>
2023-09-28 16:35:32 +0900

    0928

---
+++ boj/C_solve/11651.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+typedef struct xy {
+	int x;
+	int y;
+}	XYstruct;
+
+XYstruct XY[100001];
+
+int compare(void* first, void* second)
+{
+	XYstruct A = *(XYstruct*)first;
+	XYstruct B = *(XYstruct*)second;
+
+	if (A.y > B.y)
+		return 1;
+	else if (A.y < B.y)
+		return -1;
+	else
+	{
+		if (A.x > B.x)
+			return 1;
+		else if (A.x < B.x)
+			return -1;
+	}
+}
+
+int main(void)
+{
+	int N;
+
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++)
+		scanf("%d %d", &XY[i].x, &XY[i].y);
+
+	qsort(XY, N, sizeof(XYstruct), compare);
+
+	for (int i = 0; i < N; i++)
+		printf("%d %d\n", XY[i].x, XY[i].y);
+}
