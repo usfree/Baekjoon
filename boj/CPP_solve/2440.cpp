76abaf7381f5b9c404db2c8226326d0383e95a85
usfree <tlrmsjtm77@naver.com>
2023-10-10 21:59:09 +0900

    1010

---
+++ boj/CPP_solve/2440.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int N;
+	cin >> N;
+
+	for (int i = N; i > 0; i--)
+	{
+		for (int j = i; j > 0; j--)
+			cout << "*";
+		cout << "\n";
+	}
+}
