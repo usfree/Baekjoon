d7cb72659b475d71848d54b662f1f233bc416c69
usfree <tlrmsjtm77@naver.com>
2023-10-24 23:53:48 +0900

    1024

---
+++ boj/CPP_solve/14581.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	string yes;
+
+	cin >> yes;
+	cout << ":fan::fan::fan:" << '\n' << ":fan::" << yes << "::fan:" << '\n' << ":fan::fan::fan:";
+}
